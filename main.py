5a5eb1c099de34593b8db9647542f6ae519b82af
Kostiantyn Lytvynov <lytvynov.kostiantyn@gmail.com>
2023-03-12 22:20:29 +0200

    getting data from page

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-import re
 from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
 
+# Url for scraping
 URL = 'https://eldorado.ua/uk/'
 
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--incognito')
-options.add_argument('--headless')
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
+driver = webdriver.Chrome()
 
+# Setting max-size of screen to render all buttons
+driver.set_window_size(1920,1080)
+
+# Open the page with selenium and click js buttons to get more data
 driver.get(URL)
-html = driver.page_source
+
+# Getting the target btn and click on it
+class_name = 'iTosHu'
+target_btn = more_buttons = driver.find_elements(By.CLASS_NAME, class_name)[0]
+target_btn.click()
+sleep(5)
+
+response = driver.page_source
 driver.quit()
 
-print(html)
+# Beautiful Soup for extracting data
+soup = BeautifulSoup(response, 'lxml')
+data = soup.body
